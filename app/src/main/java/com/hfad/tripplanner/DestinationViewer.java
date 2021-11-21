7c88f8425d958d3b50ca1b790ffecf51530dfe2c
Zachary Wood <zqwood@cfl.rr.com>
2021-11-21 15:56:51 -0500

    Calvin's implementation of distance to destination

--- app/src/main/java/com/hfad/tripplanner/DestinationViewer.java
+++ app/src/main/java/com/hfad/tripplanner/DestinationViewer.java
@@ -1,23 +1,41 @@
 package com.hfad.tripplanner;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.easywaylocation.EasyWayLocation.LOCATION_SETTING_REQUEST_CODE;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class DestinationViewer extends AppCompatActivity {
+import com.example.easywaylocation.EasyWayLocation;
+import com.example.easywaylocation.Listener;
+
+import java.text.DecimalFormat;
+
+public class DestinationViewer extends AppCompatActivity implements Listener {
     Button button;
     int selectedTrip = 0;
     public static final String TRIP_ID = "tripId";
     public static final String DESTINATION_ID = "destinationId";
+
+    //    Location variables....
+    EasyWayLocation easyWayLocation;
+    private double destLat, destLon;
+
+    private TextView textViewMiles, textViewKilometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +43,8 @@ public class DestinationViewer extends AppCompatActivity {
         setContentView(R.layout.activity_destination_viewer);
 
         button = (Button) findViewById(R.id.btn_ok);
+        textViewMiles = findViewById(R.id.distance_miles);
+        textViewKilometer = findViewById(R.id.distance_kilometers);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,12 +78,12 @@ public class DestinationViewer extends AppCompatActivity {
 
             if(cursor.moveToFirst()){
                 String cityText = cursor.getString(0);
-                double lat = cursor.getDouble(1);
-                double lon = cursor.getDouble(2);
+                destLat = cursor.getDouble(1);
+                destLon = cursor.getDouble(2);
 
                 cityEdit.setText(cityText);
-                latEdit.setText(String.valueOf(lat));
-                lonEdit.setText(String.valueOf(lon));
+                latEdit.setText(String.valueOf(destLat));
+                lonEdit.setText(String.valueOf(destLon));
             }
 
 
@@ -74,6 +94,32 @@ public class DestinationViewer extends AppCompatActivity {
         } catch (SQLiteException e){
             Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
+        }
+
+        easyWayLocation = new EasyWayLocation(this, false, false, this);
+        if (isPermissionGranted()) {
+            easyWayLocation.startLocation();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+        }
+
+    }
+
+    private boolean isPermissionGranted() {
+        int permissionState = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults.length > 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    easyWayLocation.startLocation();
+                }
+            }
         }
     }
 
@@ -87,4 +133,46 @@ public class DestinationViewer extends AppCompatActivity {
         Intent intent = new Intent(this, Direction.class);
         startActivity(intent);
     }
+
+    @Override
+    public void locationOn() {
+
+    }
+
+    @Override
+    public void currentLocation(Location location) {
+        double lati = location.getLatitude();
+        double longi = location.getLongitude();
+        easyWayLocation.endUpdates();
+
+        if (destLat != 0.0 && destLon != 0.0) {
+            Location currentLocation = new Location("currentLocation");
+            currentLocation.setLatitude(lati);
+            currentLocation.setLongitude(longi);
+
+            Location destinationLocation = new Location("destinationLocation");
+            destinationLocation.setLatitude(destLat);
+            destinationLocation.setLongitude(destLon);
+
+            double distance = currentLocation.distanceTo(destinationLocation);
+            textViewKilometer.setText(new DecimalFormat("##.##").format(distance / 1000) + " KM");
+            textViewMiles.setText(new DecimalFormat("##.##").format(distance / 1609.344) + " Miles");
+        }
+    }
+
+    @Override
+    public void locationCancelled() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case LOCATION_SETTING_REQUEST_CODE:
+                easyWayLocation.onActivityResult(resultCode);
+                break;
+        }
+    }
+
 }
